e54df654e9ea13da80199bbe8f8826b7d731eba1
Maksim <codeplanetio@gmail.com>
2022-11-06 12:37:17 +0300

    Added Header, changed components

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,10 +2,6 @@ import { Input } from './Input/Input';
 import { Logo } from './Logo/Logo';
 import { Checkbox } from './Checkbox/Checkbox';
 import { Button } from './Button/Button';
+import { Header } from './Header/Header';
 
-export {
-    Input,
-    Logo,
-    Checkbox,
-    Button
-};
+export { Input, Logo, Checkbox, Button, Header };
