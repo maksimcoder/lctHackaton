0b4efe95ee4a872f0f39993d361e5ba55958d75e
Maksim <codeplanetio@gmail.com>
2022-11-06 03:47:52 +0300

    minor changes

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
-import { initCornerstone } from 'editor/cornerstone';
+// import { initCornerstone } from 'editor/cornerstone';
 
 import { router } from 'router/router';
 
@@ -19,13 +19,7 @@ import './index.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
-initCornerstone();
-
-// const req = fetch('https://s3.amazonaws.com/lury/PTCTStudy/1.3.6.1.4.1.25403.52237031786.3872.20100510032220.7.dcm');
-//         // req.then(data => console.log(data.arrayBuffer())).then(data => console.log(data));
-//         req.then(response => response.text())
-//         .then(result => console.log(result))
-//         .catch(error => console.log('error', error));
+// initCornerstone();
 
 root.render(
 	<React.StrictMode>
